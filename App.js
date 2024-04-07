368c1ab28f4d178fd47cd8d48baf44c0c9897406
Shehara Dewanagala <sahokavishka@gmail.com>
2024-04-08 01:41:32 +0530

    2nd commit
    removed templete lines and added navigation stact,native libs.... ready to design login page

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import {LoginPage} from './components/LoginPage';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import LoginPage from './components/LoginPage';
 
+const Stack=createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='LoginPage' screenOptions={{headerShown:false}}>
+        <Stack.Screen name='LoginPage' component={LoginPage}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
---
+++ components/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View ,Text, SafeAreaView} from 'react-native'
+
+function LoginPage() {
+  return (
+    <SafeAreaView>
+        <Text>LoginPage</Text>
+    </SafeAreaView>
+    
+  )
+}
+
+export default LoginPage
