92d81618a21aea4bbca12c72c4cfb1615e9c0074
Shehara Dewanagala <sahokavishka@gmail.com>
2024-04-15 22:46:31 +0530

    testing branches and  setup login page and register page

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import LoginPage from './components/LoginPage';
+import Register from './components/Register';
 
 const Stack=createNativeStackNavigator();
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='LoginPage' screenOptions={{headerShown:false}}>
         <Stack.Screen name='LoginPage' component={LoginPage}/>
+        <Stack.Screen name='SignIn' component={Register}/>
       </Stack.Navigator>
     </NavigationContainer>
     
--- components/LoginPage.js
+++ components/LoginPage.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import { View ,Text, SafeAreaView} from 'react-native'
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
 
 function LoginPage() {
+  const navigation = useNavigation();
+  const [UserEmail, onChangeUseremail] = React.useState("");
+  const [UserPassword, onChangeUserpassword] = React.useState("");
+
   return (
     <SafeAreaView>
-        <Text>LoginPage</Text>
+      <Text>LoginPage</Text>
+      <TextInput onChangeText={onChangeUseremail} value={UserEmail} />
+
+      <TextInput onChangeText={onChangeUserpassword} value={UserPassword} />
+
+      <TouchableOpacity onPress={()=>navigation.push("SignIn")}>
+        <Text>Register</Text>
+      </TouchableOpacity>
     </SafeAreaView>
-    
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
