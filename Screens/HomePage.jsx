f1ac6495f79f04efb1edca33db22129baa62dce2
Shehara Dewanagala <sahokavishka@gmail.com>
2024-04-18 09:03:42 +0530

    Update HomePage.jsx

--- Screens/HomePage.jsx
+++ Screens/HomePage.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+
 export default function HomePage() {
   return (
-    <View>
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
       <Text>HomePage</Text>
+      
     </View>
   )
 }
