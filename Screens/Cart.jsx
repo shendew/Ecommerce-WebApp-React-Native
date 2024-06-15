7378ffb185d4e3103b1418aa788dadf466eef08c
Shehara Dewanagala <sahokavishka@gmail.com>
2024-06-16 01:03:48 +0530

    Cart item bug fixed

--- Screens/Cart.jsx
+++ Screens/Cart.jsx
@@ -3,48 +3,51 @@ import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Button, FlatList, Text, View } from "react-native";
+import { Button, FlatList, SafeAreaView, Text, View } from "react-native";
 import CartItem from "./CartItem";
 import axios from "axios";
-import{BASEURL} from '@env';
+import { BASEURL } from "@env";
 
 export default function Cart() {
   const navigation = useNavigation();
-  const [auth,setAuth]=useState("");
-  const [email,setEmail]=useState("");
-  const [Products,setProducts]=useState();
-  const [QTY,setQTY]=useState();
-  const {isLoading,setIsLoading} =useState(true);
+  const [auth, setAuth] = useState("");
+  const [email, setEmail] = useState("");
+  const [Products, setProducts] = useState();
+  const [QTY, setQTY] = useState();
+  const { isLoading, setIsLoading } = useState(true);
 
   const getAuthToken = async () => {
-    
-        await AsyncStorage.getItem('AUTH_TOKEN').then(async (a) => {
-          await AsyncStorage.getItem('USER_EMAIL').then((e) => {
-            setAuth(a);
-            setEmail(e);
-            getProducts({e,a});
-          })
-        })
-
+    await AsyncStorage.getItem("AUTH_TOKEN").then(async (a) => {
+      await AsyncStorage.getItem("USER_EMAIL").then((e) => {
+        setAuth(a);
+        setEmail(e);
+        getProducts({ e, a });
+      });
+    });
   };
 
-  const getProducts = async ({e,a}) => {
+  const getProducts = async ({ e, a }) => {
     axios
-      .post(BASEURL+"/auth/cart",{
-        UserEmail:e,
-        authKey:a
-      },{
-        headers: {
-          "Content-Type": "application/json; charset=UTF-8",
+      .post(
+        BASEURL + "/auth/cart",
+        {
+          UserEmail: e,
+          authKey: a,
         },
-      })
+        {
+          headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+          },
+        }
+      )
       .then(function (response) {
-        const da=response.data.value;
-          setProducts(da)
-          
-          setQTY(response.data.cart)
-          DelayNode(1)
-          setIsLoading(false);
+        const da = response.data.value;
+
+        setQTY(response.data.cart);
+        setProducts(da);
+
+        DelayNode(1);
+        setIsLoading(false);
       })
       .catch(function (error) {
         console.log(error);
@@ -52,27 +55,29 @@ export default function Cart() {
   };
 
   useEffect(() => {
-     getAuthToken();
-    
+    getAuthToken();
   }, []);
 
-  return (
-    isLoading?<View>Loading</View>:
-    <View>
-      <FlatList
-        data={Products}
-        renderItem={({item,index})=>{
-          console.log(item)
-          console.log(index)
-          console.log(QTY)
-          return(
-            <CartItem product={item} index={index} qty={QTY}/>
-          )
-        }}
-        scrollEnabled
-      />
-
-      {/* <Button title="Place order"/> */}
-    </View>
+  return isLoading ? (
+    <View>Loading</View>
+  ) : (
+    <SafeAreaView style={{flex:1,flexDirection:'column'}}>
+      <View style={{flex:1}}>
+        <FlatList
+          data={Products}
+          renderItem={({ item, index }) => {
+            // console.log(item)
+            console.log(index);
+            // console.log(QTY)
+            return <CartItem product={item} index={index} qty={QTY} />;
+          }}
+          scrollEnabled
+        />
+      </View>
+      <View style={{paddingHorizontal:10,paddingVertical:10,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+        <Text style={{fontSize:20}}>LKR:3000</Text>
+      <Button title="Checkout"/>
+      </View>
+    </SafeAreaView>
   );
 }
--- Screens/CartItem.jsx
+++ Screens/CartItem.jsx
@@ -1,11 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, Alert } from 'react-native'
 import React from 'react'
 import { Image } from 'react-native'
+import Icon2 from "react-native-vector-icons/Feather";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function CartItem({product,index,qty}) {
-  console.log(product)
-  console.log(index)
-  console.log(qty)
+  // console.log(product)
+  // console.log(index)
+  // console.log(qty)
   return (
     <View
               style={{
@@ -13,16 +15,29 @@ export default function CartItem({product,index,qty}) {
                 backgroundColor: "white",
 
                 padding: 10,
+                marginBottom:5,
               }}
             >
+              <Icon style={{alignSelf:'flex-end',marginHorizontal:5}} name="trash" size={20} color="black" 
+              onPress={()=>{
+                Alert.alert("Delete clicked")
+              }}/>
               <View
-                style={{ borderBottomColor: "black", borderBottomWidth: 1 ,flexDirection:'row'}}
+                style={{ borderBottomColor: "black", borderBottomWidth: 1 ,flexDirection:'row',paddingBottom:5}}
               >
-                <Image height={100} width={undefined} style={{aspectRatio:1}} source={{uri:product[0].thumbnail}}/>
-                <View style={{marginLeft:5}}>
-                  <Text>{product[0].productTitle}</Text>
-                  <Text>{product[0].productPrice}</Text>
-                  <Text>{qty[index].QTY}</Text>
+                <Image height={100} width={undefined} style={{aspectRatio:1}} source={{uri:product.thumbnail}}/>
+
+                <View style={{flex:1,marginLeft:5,justifyContent:'space-between',paddingLeft:5}}>
+
+                  <View style={{flex:1,justifyContent: 'space-around'}}>
+                    <Text>{product.productTitle}</Text>
+                    <Text>x{qty[index].QTY}</Text>
+                  </View>
+
+                  <View style={{padding:10}}>
+                  <Text style={{textAlign:'right',fontSize:20,fontWeight:500}}>LKR:{product.productPrice*qty[index].QTY}</Text>
+                  </View>
+                  
 
                 </View>
               </View>
